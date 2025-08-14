38b598ba51b727fe20885af9d1c656f072ba1771
open-trade-secure <opentrade.wallet@gmail.com>
2025-08-14 00:19:20 +0000

    Corrijo base en Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/openwallet/', // O el nombre exacto de tu repo con slash inicial y final
 })
